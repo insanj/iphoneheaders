ab1356e3c36a8db6541ba0eb62bab45be354f101
insanj <insanj@gmail.com>
2014-12-02 13:10:41 -0500

    added iOS 8 version, hbang springboard headers

--- version.h
+++ version.h
@@ -58,6 +58,10 @@
 #define kCFCoreFoundationVersionNumber_iOS_7_0 847.20
 #endif
 
+#ifndef kCFCoreFoundationVersionNumber_iOS_8_0
+#define kCFCoreFoundationVersionNumber_iOS_8_0 1129.15
+#endif
+ 
 /**
  * Let's also define some useful functions to check which firmware
  * the user is on. (Note that feature detection is highly
